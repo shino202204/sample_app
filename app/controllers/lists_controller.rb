c566b7e6283983a5a7af5c5d31507db69ab43e26
shino202204 <102901960+shino202204@users.noreply.github.com>
2022-04-16 05:41:51 +0000

    [Add]form_withにオプション・createアクションにコメントを追加

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,6 +8,7 @@ class ListsController < ApplicationController
     # データを受け取り新規登録するためのインスタンス作成
     list = List.new(list_params)
     # データをDBに保存するためのsaveメソッド実行
+    # saveメソッド：呼び出したModelインスタンスをDBへ保存する
     list.save
     # トップ画面へリダイレクト
     redirect_to '/top'
@@ -22,8 +23,16 @@ class ListsController < ApplicationController
   def edit
   end
 
+  # privateの行より後に定義されたメソッドは、アクションとして認識されなくなり、URLと対応できなくなる
   private
+
   # ストロングパラメータ
+  # マスアサインメント脆弱性を防ぐ（保存カラムを限定することにより）
+  # params：formから送られてくるデータが中に入っている
+  # require：モデル名を指定する
+  # permit：保存を許可するカラムを指定する
+
+  # privateメソッドの名前：「モデル名_params」とすることが多い
   def list_params
     params.require(:list).permit(:title, :body)
   end
